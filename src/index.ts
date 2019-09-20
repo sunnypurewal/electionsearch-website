33f833f847f0d88ddeed8fc6a41bc540af47ae29
sunny <sunnypurewal@gmail.com>
2019-09-20 11:49:19 -0400

    Added spell check autocorrect to searching

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,23 @@
 import express = require("express")
 import * as path from "path"
-import got = require("got")
+import search from "./search"
 
-const app = express();
-app.set("view engine", "ejs");
-app.use(express.static("static"));
-const port = 8080;
+const app = express()
+app.set("view engine", "ejs")
+app.use(express.static("static"))
+const port = 8080
 
 app.get("/", (_, res: express.Response) => {
-  res.sendFile(path.join(__dirname + "/static/index.html"));
-});
+  res.sendFile(path.join(__dirname + "/static/index.html"))
+})
 
 app.get("/search", (req: express.Request, res: express.Response) => {
-  (async () => {
-    try {
-      const response = await got(`http://localhost:9200/articles/_search?q=${req.query.q}`);
-
-      res.render("search", {articles: JSON.parse(response.body)})
-    } catch (error) {
-      console.log(error.response.body);
-    }
-  })();
-});
+  search(req.query.q)
+  .then((json: any) => {
+    res.render("search", {articles: json})
+  })
+})
 
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Server started on ${port}`);
-});
+  console.log(`Server started on ${port}`)
+})
---
+++ src/search.ts
@@ -0,0 +1,59 @@
+import fetch from "node-fetch"
+
+export default function search(query: string, retry: number = 0, resolver?: (value?: unknown) => void) {
+  return new Promise((resolve, _) => {
+    const body = {
+      query: {
+          bool : {
+              must : {
+                  query_string : {
+                      query,
+                  },
+              },
+          },
+      },
+      suggest: {
+        corrections: {
+          term: {
+            field: "body",
+            sort: "frequency",
+          },
+          text: query,
+        },
+      },
+    }
+    fetch(`http://localhost:9200/articles/_search`,
+    {
+      body: JSON.stringify(body),
+      headers: {"Content-Type": "application/json"},
+      method: "post",
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.hits.hits.length > 0) {
+        if (resolver) {
+          resolver(json)
+        } else {
+          resolve(json)
+        }
+      } else {
+        const corrections = json.suggest.corrections as any[]
+        if (!retry) {
+          const corrected: string[] = []
+          corrections.forEach((c) => {
+            if (c.options.length > 0) {
+              corrected.push(c.options[0].text)
+            }
+          })
+          if (corrected.length === query.split(" ").length) {
+            search(corrected.join(" "), retry + 1, resolve)
+          } else {
+            resolve(json)
+          }
+        } else {
+          resolve(json)
+        }
+      }
+    })
+  })
+}
